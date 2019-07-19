ef49dd403fcc7f58d7437baf412dd915449517cc
RichieMay <myanbu@163.com>
2019-07-19 12:12:10 +0800

    Create FullScreen.user.js

---
+++ FullScreen.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         启用视频全屏
+// @namespace    https://github.com/RichieMay/FullScreen
+// @version      1.1
+// @description  启用视频全屏
+// @author       RichieMay
+// @match        http*://*.pupudy.com/*
+// @match        http*://*.tt27.tv/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var iframe = document.getElementsByTagName('iframe');
+    for (var i = 0; i < iframe.length; i++) {
+        iframe[i].setAttribute('allowFullScreen', 'true');
+    }
+})();

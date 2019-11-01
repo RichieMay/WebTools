3d8ed3831218c39c2996a6d630fe83da6e1fcda6
RichieMay <myanbu@163.com>
2019-11-02 00:36:51 +0800

    update Global.user.js

---
+++ Global.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         全局工具集
+// @namespace    https://github.com/RichieMay/WebTools/raw/master/Global.user.js
+// @version      1.0
+// @description  全局工具集，包含视频全屏、禁止浏览器自动添加搜索引擎
+// @author       RichieMay
+// @grant        none
+// @match        *
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function EnableFullScreen() {
+        [].forEach.call(document.getElementsByTagName('iframe'), function(iframe){
+            iframe.setAttribute('allowFullScreen', 'true');
+        });
+    }
+
+    function DisableAddSearchEngines() {
+        [].forEach.call(document.querySelectorAll('[type="application/opensearchdescription+xml"]'), function(openSearch) {
+            openSearch.remove();
+        });
+    }
+
+    EnableFullScreen();
+
+    DisableAddSearchEngines();
+
+})();

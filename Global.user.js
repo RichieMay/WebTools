aa0f8c116f8b3594d6fb54730116f60f30f86a66
RichieMay <myanbu@163.com>
2023-02-20 12:40:15 +0800

    Update Global.user.js

--- Global.user.js
+++ Global.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         全局工具集
 // @namespace    https://github.com/RichieMay/WebTools/raw/master/Global.user.js
-// @version      1.4
+// @version      1.5
 // @description  全局工具集，包含视频全屏、禁止浏览器自动添加搜索引擎、it1352.com网站免登录查看问题答案
 // @author       RichieMay
 // @grant        none

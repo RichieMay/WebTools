426406931eeb5ed5cc39833e7b961a0f9de5b7dc
RichieMay <myanbu@163.com>
2024-04-16 14:13:29 +0800

    Update ExtendCoze.user.js

--- ExtendCoze.user.js
+++ ExtendCoze.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ExtendCozeChat
 // @namespace    https://github.com/RichieMay/WebTools/raw/master/ExtendCoze.user.js
-// @version      1.0.1
+// @version      1.0.2
 // @description  扩展Coze聊天页
 // @author       RichieMay
 // @match        http*://*.coze.com/*
@@ -20,8 +20,8 @@
               if (containers.length > 0) {
                   if (containers[0].getAttribute('extend') == null) {
                       containers[0].setAttribute('extend', true)
-                        containers[0].setAttribute('style', 'grid-template-columns:25fr 70fr 5fr;')
-                      
+                      containers[0].setAttribute('style', 'grid-template-columns:25fr 70fr 5fr;')
+
                       containers[0].children[0].style.position = 'fixed'
                       containers[0].children[0].style.visibility = 'hidden'
                   }

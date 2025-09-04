c177e3cb23afb7c1fc995d5cefae91bc4c0c457a
RichieMay <myanbu@163.com>
2025-09-04 18:39:22 +0800

    Update ZZSpider.user.js

--- ZZSpider.user.js
+++ ZZSpider.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         转转商品爬虫
 // @namespace    https://github.com/RichieMay/WebTools/raw/master/ZZSpider.user.js
-// @version      1.0.9
+// @version      1.0.10
 // @description  转转商品爬虫
 // @author       RichieMay
 // @match        https://m.zhuanzhuan.com/*
@@ -12,101 +12,107 @@
 (function() {
     'use strict';
 
-    const methods = {refresh: () => { console.debug(new Date().toLocaleString(), 'spider refresh ...'); window.scrollTo(0, document.body.scrollHeight); }};
-    const worker = new Worker(URL.createObjectURL(new Blob([`
+    window.backend = new Worker(URL.createObjectURL(new Blob([`
             function add_to_favorites(good) {
-                fetch('https://app.zhuanzhuan.com/zz/transfer/addLoveInfo?infoId=' + good.id + '&metric=' + good.metric, {method: 'GET',credentials: 'include'});
+                fetch('https://app.zhuanzhuan.com/zz/transfer/addLoveInfo?infoId=' + good.id + '&metric=' + good.metric, {method: 'GET', credentials: 'include'});
             }
 
-            async function parse_os_version(unique, good) {
+            function parse_os_version(unique, good) {
                 if (unique.includes(good.id)) {
                     console.error('重复商品ID: ' + good.id + ', 标题: ' + good.title);
                     return;
                 }
 
                 unique.push(good.id);
-                const response = await fetch('https://app.zhuanzhuan.com/zzopen/waresshow/moreInfo?infoId=' + good.id, {method: 'GET',credentials: 'include'});
-                const result = await response.json();
-                for (const param of result.respData.report.params) {
-                    if (param.key === '系统版本') {
-                        if (/.+17\.0$/.test(param.value)) {
+                return fetch('https://app.zhuanzhuan.com/zzopen/waresshow/moreInfo?infoId=' + good.id, {method: 'GET', credentials: 'include'})
+                    .then(res => res.json())
+                    .then(body => {
+                            if (!Array.from(body.respData.report.params).find(param => param.key === '系统版本' && /.+17\.0$/.test(param.value))) {
+                                return null;
+                            }
+
                             console.warn('商品链接: https://m.zhuanzhuan.com/u/streamline_detail/new-goods-detail?infoId=' + good.id);
                             add_to_favorites(good);
-                            break;
-                        }
-                    }
-                }
+                            return good;
+                    }).catch(e => null);
             }
 
-            const global = {queue:[],  unique:[], refresh: 1};
+            function load_more_goods(entry) {
+                entry.body.param.pageIndex += 1;
+                entry.body.param.rstmark = Date.now();
+                entry.request_headers.zzreqt = Date.now();
+
+                const formData = new URLSearchParams();
+                formData.append('param', JSON.stringify(entry.body.param));
+                return fetch(entry.url, {method: entry.method, headers: entry.request_headers, body: formData, credentials: 'include'})
+                    .then(res => res.json())
+                    .then(body => {
+                        return Array.from(body.respData.datas).map(good => {
+                            return {id: good.infoId, title: good.title, metric: good.metricValue};
+                        });
+                    }).catch(e => []);
+            }
+
+            const global = {queue:[], unique:[], entry: {}, timer:-1, complete: true};
             const methods = {
                 start: () => {
+                    if (global.timer != -1) { return; };
+
+                    console.clear();
                     console.debug(new Date().toLocaleString(), 'spider start ...');
 
-                    setInterval(() => {
-                        if (global.queue.length != 0) {
+                    global.timer = setInterval(() => {
+                        if (global.complete) {
                             try {
-                                global.refresh = 0;
-                                parse_os_version(global.unique, global.queue.shift());
-                            } catch {}
-                        } else if (global.refresh === 0) {
-                            global.refresh = 1;
-                            self.postMessage({method: 'refresh', args: []});
-                        } else if (global.refresh === 1) {
-                            global.refresh = -1;
-                            console.debug(new Date().toLocaleString(), 'spider is waiting for update ...');
+                                global.complete = false;
+                                if (global.queue.length != 0) {
+                                    parse_os_version(global.unique, global.queue.shift()).then(good => {global.complete = true;});
+                                } else {
+                                    load_more_goods(global.entry).then(goods => {
+                                        global.complete = true;
+                                        global.queue.push(...goods);
+                                    });
+                                }
+                            } catch {
+                                global.complete = true;
+                            }
                         }
                     }, 1000);
                 },
 
-                update: (goods) => {
-                    console.debug(new Date().toLocaleString(), 'spider update ...', global.queue.length, goods.length);
+                stop: () => {
+                    if (global.timer == -1) { return; }
+                    console.debug(new Date().toLocaleString(), 'spider stop ...');
 
-                    global.queue.push(...goods);
+                    clearInterval(global.timer);
+                    global.timer = -1;
+                },
+
+                sync: (entry) => {
+                    console.debug(new Date().toLocaleString(), 'spider sync ...');
+
+                    const data = entry.body.split('=');
+                    entry.body = {[data[0]]: JSON.parse(decodeURIComponent(data[1]))};
+                    entry.body.param.pageIndex -= 1;
+
+                    global.queue = [];
+                    global.entry = entry;
                 }
             };
 
             self.onmessage = (e) => {
-                methods[e.data.method].apply(methods, e.data.args);
+                methods[e.data.method].apply(methods, 'args' in e.data ? e.data.args : []);
             };
-
         `], {type: 'application/javascript'})));
 
-    worker.onmessage = (e) => {
-        methods[e.data.method].apply(methods, e.data.args);
+    XMLHttpRequest.prototype._send = XMLHttpRequest.prototype.send;
+    XMLHttpRequest.prototype.send = function(...args) {
+        if (this.__sentry_xhr_v3__.url.includes('/zzopen/ypmall/listData')) {
+            window.backend.postMessage({method: 'sync', args: [this.__sentry_xhr_v3__]});
+        }
+
+        return this._send(...args);
     };
 
-    function setup_goods_node(node)
-    {
-        new MutationObserver(function(mutations) {
-            worker.postMessage({
-                method: 'update',
-                args: [
-                    Array.from(mutations)
-                    .flatMap(mutation => Array.from(mutation.addedNodes))
-                    .filter(node => node.nodeType === Node.ELEMENT_NODE && node.hasAttribute('zz-infoid'))
-                    .map(node => ({
-                        id: node.getAttribute('zz-infoid'),
-                        title: node.getAttribute('zz-sortname'),
-                        metric: node.getAttribute('data-metric')
-                    }))
-                ]
-            });
-        }).observe(node, {childList : true});
-
-        console.clear();
-        worker.postMessage({method: 'start', args: []});
-    }
-
-    new MutationObserver(function(mutations, observer) {
-        for (const mutation of mutations) {
-            for (const node of mutation.addedNodes) {
-                try {
-                    setup_goods_node(node.querySelector('div[zz-sectionid="111"]'));
-                    observer.disconnect();
-                    return;
-                } catch {}
-            }
-        }
-    }).observe(document.getElementById('app'), {childList : true});
+    window.backend.postMessage({method: 'start'});
 })();

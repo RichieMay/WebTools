6b54cd153392c0db279dfdefc2eebb3316122498
RichieMay <myanbu@163.com>
2025-09-04 10:58:41 +0800

    Update ZZSpider.user.js

--- ZZSpider.user.js
+++ ZZSpider.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         转转商品爬虫
 // @namespace    https://github.com/RichieMay/WebTools/raw/master/ZZSpider.user.js
-// @version      1.0.8
+// @version      1.0.9
 // @description  转转商品爬虫
 // @author       RichieMay
 // @match        https://m.zhuanzhuan.com/*
 // @grant        none
-// @run-at       document-end
+// @run-at       document-start
 // ==/UserScript==
 
 (function() {

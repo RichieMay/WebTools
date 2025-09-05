72809bbda796f83d05d6c926b92fd0cea8c76b2f
RichieMay <myanbu@163.com>
2025-09-05 09:12:51 +0800

    Update ZZSpider.user.js

--- ZZSpider.user.js
+++ ZZSpider.user.js
@@ -57,6 +57,8 @@
             const methods = {
                 start: () => {
                     if (global.timer != -1) { return; };
+
+                    console.clear();
                     console.debug(new Date().toLocaleString(), 'spider start ...');
 
                     global.timer = setInterval(() => {
@@ -100,7 +102,6 @@
                     const data = entry.body.split('=');
                     entry.body = {[data[0]]: JSON.parse(decodeURIComponent(data[1]))};
                     if (entry.body.param.pageIndex == 1) {
-                        console.clear();
                         global.queue = [];
                         global.entry = entry;
                         global.entry.body.param.pageIndex = 0;
